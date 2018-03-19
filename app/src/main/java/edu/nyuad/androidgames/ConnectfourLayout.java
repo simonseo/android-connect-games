7788616860ebe716fe79c0b2841c8f8c11efbef9
Simon Myunggun Seo <ms9144@nyu.edu>
2018-03-19 14:30:32 +0400

    Add game layouts and update gradle

---
+++ app/src/main/java/edu/nyuad/androidgames/ConnectfourLayout.java
@@ -0,0 +1,23 @@
+package edu.nyuad.androidgames;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by seo on 3/7/18.
+ */
+
+public class ConnectfourLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.connectfour_layout, container, false);
+        return v;
+        //        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
