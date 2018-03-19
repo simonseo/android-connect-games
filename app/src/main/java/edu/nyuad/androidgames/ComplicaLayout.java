5e26a578865a75cade0ed82e4050518dd1c9fdfc
Simon Myunggun Seo <ms9144@nyu.edu>
2018-03-19 15:27:42 +0400

    Roll back and add buttons

--- app/src/main/java/edu/nyuad/androidgames/ComplicaLayout.java
+++ app/src/main/java/edu/nyuad/androidgames/ComplicaLayout.java
@@ -12,14 +12,14 @@ import edu.nyuad.androidgames.R;
 /**
  * Created by seo on 3/7/18.
  */
-
-public class ComplicaLayout extends Fragment {
-    View v;
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.complica_layout, container, false);
-        return v;
-        //        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-}
+//
+//public class ComplicaLayout extends Fragment {
+//    View v;
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+//        v = inflater.inflate(R.layout.complica_layout, container, false);
+//        return v;
+//        //        return super.onCreateView(inflater, container, savedInstanceState);
+//    }
+//}
